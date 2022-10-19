eced429edc421d056ec01ab2c20a5ed39062e976
Oli Smith <oliver.smith@avanade.com>
2022-10-19 16:45:33 +0100

    made strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+stringy_boi = "hi there i'm an array what's a string."
+stringy_boi_2 = 'what the hell is an array I am a string.'
+stringy_G = 'woah guys chill out ' + "don't get yourself in a twist haha get it."
+stringy_boi_response = 'no' + ' ' + 'what' + ' ' + 'are' + ' ' + 'you' + ' ' + 'on' + ' ' + 'about'
+stringy_G = "cause we're all messed up"
+print('no we are not')
+stringy_boi_2_2 = 'what the hell was that?'
+print('nothing')
+print(stringy_boi + stringy_boi_2)
+stringy_G_ = "oh my god they've been merged I'm going to throw up"
