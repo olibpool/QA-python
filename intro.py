9366da46f37266b99b687e3508778869cea8ca28
Oli Smith <oliver.smith@avanade.com>
2022-10-19 16:37:56 +0100

    finished the first exercies

---
+++ intro.py
@@ -0,0 +1,10 @@
+english = int(input('Input your english score now: '))
+maths = int(input('Input your maths score now: '))
+science = int(input('Input your science score now: '))
+
+total = english + maths + science
+
+average = total / 3
+
+print('Your average score is:')
+print(average)
