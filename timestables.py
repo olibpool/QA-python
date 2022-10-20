97a91f31853cff598a15dec2cece6a6c90a93a0f
Oli Smith <oliver.smith@avanade.com>
2022-10-20 15:45:44 +0100

    messin

--- timestables.py
+++ timestables.py
@@ -1,3 +1,15 @@
-for i in range(1,11):
-    for j in range(1,11):
-        print(f"{str(i)}*{str(j)}={i*j}")
+import timeit
+
+
+def timestables():
+    for i in range(1,11):
+        for j in range(1,11):
+            print(f"{str(i)}*{str(j)}={i*j}")
+            
+    print('\n Time taken: ')
+                    
+loop = 100
+
+
+result = timeit.timeit('timestables()', globals=globals(), number=loop)
+print(result / loop)
