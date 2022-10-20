542c719355da98e83eb88f1a866f7083addbefac
Oli Smith <oliver.smith@avanade.com>
2022-10-20 16:45:18 +0100

    did books task

---
+++ books.py
@@ -0,0 +1,23 @@
+books = {
+        'JK Rowling': ['Harry Potter & the Philoshopers stone', 'Harry Potter & the Chamber of Secrets', 'Harry Potter & Prisoner of Askaban'],
+        'Stephen King': ['IT', 'The Ring'],
+        'Stephen Hawking': ['A short history of Time', 'A short history of Time'],
+        'JRR Tolkein': ['The Lord of the Rings, The Two Towers', 'The Lord of the Rings, Entrance to Mordor']
+    }
+
+
+def books_function():
+    author = input("Input an author's name: ")
+    
+    output = ''
+    
+    for book in sorted(books[author]):
+        output += book + ', '
+    
+    return output
+
+
+print(books_function())
+    
+    
+    
