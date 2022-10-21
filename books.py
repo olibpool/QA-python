55e349a9cb92484aa01089d9363653c0f160db27
Oli Smith <oliver.smith@avanade.com>
2022-10-21 09:16:17 +0100

    modified books

--- books.py
+++ books.py
@@ -2,7 +2,8 @@ books = {
         'JK Rowling': ['Harry Potter & the Philoshopers stone', 'Harry Potter & the Chamber of Secrets', 'Harry Potter & Prisoner of Askaban'],
         'Stephen King': ['IT', 'The Ring'],
         'Stephen Hawking': ['A short history of Time', 'A short history of Time'],
-        'JRR Tolkein': ['The Lord of the Rings, The Two Towers', 'The Lord of the Rings, Entrance to Mordor']
+        'JRR Tolkein': ['The Lord of the Rings, The Two Towers', 'The Lord of the Rings, Entrance to Mordor'],
+        'Oli': ['Olis big book of cool facts about himself']
     }
 
 
